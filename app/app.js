3b6b06f81fd402369d1d45f6a097f62f7084ad4b
Albo Vieira <albovieira@gmail.com>
2016-08-03 22:41:21 -0300

    firebase integration

--- app/app.js
+++ app/app.js
@@ -95,6 +95,42 @@ var chatComponent = Vue.extend({
 });
 
 //rooms
+var rooms = [
+    {id: "001", name: "PHP", description: "Sala PHP"},
+    {id: "002", name: "JAVA", description: "Sala JAVA"},
+    {id: "003", name: "PYTHON", description: "Sala PYTHON"},
+    {id: "004", name: "JAVASCRIPT", description: "Sala JAVASCRIPT"},
+    {id: "005", name: "RUBY", description: "Sala RUBY"}
+];
+var roomsCreateComponent = Vue.extend({
+    template: `
+    <p>Salas Criadas:</p>
+    <ul>
+        <li v-for="r in rooms">
+        {{r.name}}
+        </li>
+    </ul>
+    `,
+
+    firebase: {
+        rooms: db.ref('chat/rooms')
+    },
+
+    ready: function () {
+        var chatRef = db.ref('chat');
+        var roomsRef = chatRef.child('rooms');
+
+        //persist in firebase the array rooms
+        rooms.forEach(function (room) {
+            roomsRef.child(room.id).set({
+                name: room.name,
+                description: room.description
+            })
+        });
+
+    }
+});
+
 var roomsComponent = Vue.extend({
     template: `
         <div class="col-md-4" v-for="r in rooms">
@@ -136,13 +172,14 @@ var roomsComponent = Vue.extend({
             this.$route.router.go('/chat/'+ room.id);
         },
         insertData: function () {
-          this.$firebaseRefs.array.push({
-              text: this.text
-          });
+            this.$firebaseRefs.array.push({
+                text: this.text
+            });
         }
     }
 
 });
+
 
 //app
 var appComponent = Vue.extend({});
@@ -155,6 +192,9 @@ router.map({
     },
     '/rooms': {
         component: roomsComponent
+    },
+    '/rooms/create': {
+        component: roomsCreateComponent
     }
 });
 
